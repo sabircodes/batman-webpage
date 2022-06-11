cf8d440db25c16288812ca0631fd0d667604eb45
sabircodes <smdakhtar007@gmail.com>
2022-06-12 02:55:58 +0530

    new addition

---
+++ index.js
@@ -0,0 +1,8 @@
+function myfun(){
+    var h = document.querySelector("#home");
+    h.style.backgroundImage="url(b1.jpg)";
+}
+function myfun2(){
+    var h2 = document.querySelector("#home");
+    h2.style.backgroundImage="url('bw.jpg')";
+}
